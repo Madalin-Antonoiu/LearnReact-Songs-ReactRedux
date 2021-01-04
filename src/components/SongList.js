b0a6a779d1e9a703c4d9fbd5fcd9e9e02878de65
@madalin-antonoiu <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-01-04 22:02:26 +0000

    Linked store with my component to get data

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,9 +3,14 @@ import { connect } from "react-redux";
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
     return <div>SongList</div>;
   }
 }
 
-export default connect()(SongList);
-// connect() () - first time it returns a function, then calls the returned function - valid JS Code
+// Basically it takes data from Redux Store and puts it as this.state.songs :)
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
